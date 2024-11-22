3b5afd622cb81f977dfdba055a0b2f54c654927a
Arnav12S <Arnav12S@users.noreply.github.com>
2024-11-22 09:53:41 +0000

    deploy: 6834a7504a9b3247c23d1ddcc8433fb79f214826

--- run/elementary/models/edr/dbt_artifacts/dbt_metrics.sql
+++ run/elementary/models/edr/dbt_artifacts/dbt_metrics.sql
@@ -5,7 +5,7 @@
             where (
                 unique_id) in (
                 select (unique_id)
-                from "dbt_metrics__dbt_tmp093231865341"
+                from "dbt_metrics__dbt_tmp095305061022"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "neondb"."elementary"."dbt_metrics" ("unique_id", "name", "label", "model", "type", "sql", "timestamp", "filters", "time_grains", "dimensions", "depends_on_macros", "depends_on_nodes", "description", "tags", "meta", "package_name", "original_path", "path", "generated_at", "metadata_hash")
     (
         select "unique_id", "name", "label", "model", "type", "sql", "timestamp", "filters", "time_grains", "dimensions", "depends_on_macros", "depends_on_nodes", "description", "tags", "meta", "package_name", "original_path", "path", "generated_at", "metadata_hash"
-        from "dbt_metrics__dbt_tmp093231865341"
+        from "dbt_metrics__dbt_tmp095305061022"
     )
   
--- run/elementary/models/edr/run_results/test_result_rows.sql
+++ run/elementary/models/edr/run_results/test_result_rows.sql
@@ -5,7 +5,7 @@
             where (
                 elementary_test_results_id) in (
                 select (elementary_test_results_id)
-                from "test_result_rows__dbt_tmp093247273520"
+                from "test_result_rows__dbt_tmp095317490225"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "neondb"."elementary"."test_result_rows" ("elementary_test_results_id", "result_row", "detected_at", "created_at")
     (
         select "elementary_test_results_id", "result_row", "detected_at", "created_at"
-        from "test_result_rows__dbt_tmp093247273520"
+        from "test_result_rows__dbt_tmp095317490225"
     )
   
