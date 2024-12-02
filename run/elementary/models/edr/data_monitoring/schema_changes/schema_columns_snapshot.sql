ce8a7747b12d5ce9d02de16ca7a9986a6290aee3
Arnav12S <Arnav12S@users.noreply.github.com>
2024-12-02 12:31:56 +0000

    deploy: 413edab5daabca4d66f8038eb6e0f2d97fb4d3c3

--- run/elementary/models/edr/data_monitoring/schema_changes/schema_columns_snapshot.sql
+++ run/elementary/models/edr/data_monitoring/schema_changes/schema_columns_snapshot.sql
@@ -5,7 +5,7 @@
             where (
                 column_state_id) in (
                 select (column_state_id)
-                from "schema_columns_snapshot__dbt_tmp122508124227"
+                from "schema_columns_snapshot__dbt_tmp123036797084"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "postgres"."elementary"."schema_columns_snapshot" ("column_state_id", "full_column_name", "full_table_name", "column_name", "data_type", "is_new", "detected_at", "created_at")
     (
         select "column_state_id", "full_column_name", "full_table_name", "column_name", "data_type", "is_new", "detected_at", "created_at"
-        from "schema_columns_snapshot__dbt_tmp122508124227"
+        from "schema_columns_snapshot__dbt_tmp123036797084"
     )
   
--- run/elementary/models/edr/dbt_artifacts/dbt_tests.sql
+++ run/elementary/models/edr/dbt_artifacts/dbt_tests.sql
@@ -5,7 +5,7 @@
             where (
                 unique_id) in (
                 select (unique_id)
-                from "dbt_tests__dbt_tmp122505883077"
+                from "dbt_tests__dbt_tmp123034350594"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "postgres"."elementary"."dbt_tests" ("unique_id", "database_name", "schema_name", "name", "short_name", "alias", "test_column_name", "severity", "warn_if", "error_if", "test_params", "test_namespace", "test_original_name", "tags", "model_tags", "model_owners", "meta", "depends_on_macros", "depends_on_nodes", "parent_model_unique_id", "description", "package_name", "type", "original_path", "path", "generated_at", "metadata_hash", "quality_dimension")
     (
         select "unique_id", "database_name", "schema_name", "name", "short_name", "alias", "test_column_name", "severity", "warn_if", "error_if", "test_params", "test_namespace", "test_original_name", "tags", "model_tags", "model_owners", "meta", "depends_on_macros", "depends_on_nodes", "parent_model_unique_id", "description", "package_name", "type", "original_path", "path", "generated_at", "metadata_hash", "quality_dimension"
-        from "dbt_tests__dbt_tmp122505883077"
+        from "dbt_tests__dbt_tmp123034350594"
     )
   
