a1faf3fd76c2d5b0d3397b6f73033d45a7310c8c
Arnav12S <Arnav12S@users.noreply.github.com>
2024-12-02 12:26:24 +0000

    deploy: 413edab5daabca4d66f8038eb6e0f2d97fb4d3c3

--- run/elementary/models/edr/dbt_artifacts/dbt_metrics.sql
+++ run/elementary/models/edr/dbt_artifacts/dbt_metrics.sql
@@ -5,7 +5,7 @@
             where (
                 unique_id) in (
                 select (unique_id)
-                from "dbt_metrics__dbt_tmp121334401568"
+                from "dbt_metrics__dbt_tmp122438355022"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "postgres"."elementary"."dbt_metrics" ("unique_id", "name", "label", "model", "type", "sql", "timestamp", "filters", "time_grains", "dimensions", "depends_on_macros", "depends_on_nodes", "description", "tags", "meta", "package_name", "original_path", "path", "generated_at", "metadata_hash")
     (
         select "unique_id", "name", "label", "model", "type", "sql", "timestamp", "filters", "time_grains", "dimensions", "depends_on_macros", "depends_on_nodes", "description", "tags", "meta", "package_name", "original_path", "path", "generated_at", "metadata_hash"
-        from "dbt_metrics__dbt_tmp121334401568"
+        from "dbt_metrics__dbt_tmp122438355022"
     )
   
