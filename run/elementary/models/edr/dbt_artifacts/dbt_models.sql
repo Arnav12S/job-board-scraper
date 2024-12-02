9ad1e997ffe5a83db9dac299566ca5b029437897
Arnav12S <Arnav12S@users.noreply.github.com>
2024-12-02 12:50:10 +0000

    deploy: 0c8d6b33835b72723860a29626cffd312c9361ad

--- run/elementary/models/edr/dbt_artifacts/dbt_models.sql
+++ run/elementary/models/edr/dbt_artifacts/dbt_models.sql
@@ -5,7 +5,7 @@
             where (
                 unique_id) in (
                 select (unique_id)
-                from "dbt_models__dbt_tmp124451326774"
+                from "dbt_models__dbt_tmp124848300517"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "postgres"."elementary"."dbt_models" ("unique_id", "alias", "checksum", "materialization", "tags", "meta", "owner", "database_name", "schema_name", "depends_on_macros", "depends_on_nodes", "description", "name", "package_name", "original_path", "path", "patch_path", "generated_at", "metadata_hash", "unique_key", "incremental_strategy")
     (
         select "unique_id", "alias", "checksum", "materialization", "tags", "meta", "owner", "database_name", "schema_name", "depends_on_macros", "depends_on_nodes", "description", "name", "package_name", "original_path", "path", "patch_path", "generated_at", "metadata_hash", "unique_key", "incremental_strategy"
-        from "dbt_models__dbt_tmp124451326774"
+        from "dbt_models__dbt_tmp124848300517"
     )
   
